ea6346d7ac7c247cdc5aeef82a4ecc11291f9acb
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-09 20:14:23 +0330

    phase 1: changed the lombok annotations of BaseEntity to be more explicit

--- phase_01/src/main/java/entity/base/BaseEntity.java
+++ phase_01/src/main/java/entity/base/BaseEntity.java
@@ -1,19 +1,20 @@
 package entity.base;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 
 @MappedSuperclass
-@Data
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "id_generator")
-    protected Long id;
+    private Long id;
 }
