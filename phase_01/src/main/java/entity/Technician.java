20adf0a75d4547bf60d3b9bf3da7124880745956
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-09 20:16:38 +0330

    phase 1: added many to many relationship between Technician and SubAssistance

--- phase_01/src/main/java/entity/Technician.java
+++ phase_01/src/main/java/entity/Technician.java
@@ -7,6 +7,8 @@ import lombok.ToString;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("Technician")
@@ -17,4 +19,6 @@ import javax.persistence.Entity;
 @EqualsAndHashCode(callSuper = true)
 public class Technician extends Person{
     private long credit;
+    @ManyToMany(mappedBy = "technicians")
+    private List<SubAssistance> subAssistances;
 }
