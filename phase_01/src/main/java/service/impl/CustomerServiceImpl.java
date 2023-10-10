f01b0cbf007a9b05d0a0b88a20f9fab2832dc671
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-10 19:04:35 +0330

    phase 1: added methods to CustomerServiceImpl and TechnicianServiceImpl to get their field when saving

--- phase_01/src/main/java/service/impl/CustomerServiceImpl.java
+++ phase_01/src/main/java/service/impl/CustomerServiceImpl.java
@@ -4,9 +4,30 @@ import basics.baseService.impl.BaseServiceImpl;
 import entity.Customer;
 import repository.impl.CustomerRepositoryImpl;
 
+import java.time.LocalDate;
+
 public class CustomerServiceImpl extends BaseServiceImpl<CustomerRepositoryImpl, Customer> {
 
     public CustomerServiceImpl(CustomerRepositoryImpl repository) {
         super(repository);
+    }
+
+    public Customer specifyCustomer(){
+        printer.getInput("first name");
+        String firstname = input.nextLine();
+        printer.getInput("last name");
+        String lastname = input.nextLine();
+        printer.getInput("email");
+        String email = input.nextLine();
+        printer.getInput("user name");
+        String username = input.nextLine();
+        printer.getInput("password");
+        String password = input.nextLine();
+        LocalDate registrationDate = LocalDate.now();
+        printer.getInput("initial credit");
+        long credit = input.nextLong();
+        input.nextLine();
+        return Customer.builder().firstName(firstname).lastName(lastname).email(email).username(username)
+                .password(password).registrationDate(registrationDate).credit(credit).isCustomer(true).build();
     }
 }
--- phase_01/src/main/java/service/impl/TechnicianServiceImpl.java
+++ phase_01/src/main/java/service/impl/TechnicianServiceImpl.java
@@ -2,11 +2,60 @@ package service.impl;
 
 import basics.baseService.impl.BaseServiceImpl;
 import entity.Technician;
+import entity.enums.TechnicianStatus;
+import exceptions.InvalidImageException;
 import repository.impl.TechnicianRepositoryImpl;
+import utility.Constants;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.List;
 
 public class TechnicianServiceImpl extends BaseServiceImpl<TechnicianRepositoryImpl, Technician> {
 
     public TechnicianServiceImpl(TechnicianRepositoryImpl repository) {
         super(repository);
+    }
+
+    public Technician specifyTechnician(Path path){
+        printer.getInput("first name");
+        String firstname = input.nextLine();
+        printer.getInput("last name");
+        String lastname = input.nextLine();
+        printer.getInput("email");
+        String email = input.nextLine();
+        printer.getInput("user name");
+        String username = input.nextLine();
+        printer.getInput("password");
+        String password = input.nextLine();
+        LocalDate registrationDate = LocalDate.now();
+        try {
+            byte[] image = Files.readAllBytes(path);
+            return Technician.builder().firstName(firstname).lastName(lastname).email(email).username(username)
+                    .password(password).registrationDate(registrationDate).score(0).credit(0).isActive(true)
+                    .technicianStatus(TechnicianStatus.PENDING).subAssistances(List.of())
+                    .image(image).isTechnician(true).build();
+        } catch (IOException e) {
+            printer.printError(e.getMessage());
+            return null;
+        }
+
+    }
+
+    public boolean validateImage(Path path){
+        try {
+            String pathString = path.toString();
+            if(!pathString.endsWith(".jpg"))
+                throw new InvalidImageException(Constants.INVALID_IMAGE_FORMAT);
+            byte[] image = Files.readAllBytes(path);
+            if(image.length > 307200)
+                throw new InvalidImageException(Constants.INVALID_IMAGE_SIZE);
+            return true;
+        } catch (IOException | InvalidImageException e) {
+            printer.printError(e.getMessage());
+            return false;
+        }
     }
 }
