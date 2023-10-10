fea74bdfd5549f41f39b0e883c6361c2033661bd
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-11 01:36:47 +0330

    phase 1: added new exception messages

--- phase_01/src/main/java/utility/Constants.java
+++ phase_01/src/main/java/utility/Constants.java
@@ -6,4 +6,8 @@ public class Constants {
     public static final String INCORRECT_PASSWORD = "Incorrect current password";
     public static final String INVALID_USERNAME = "No person found with this username";
     public static final String INCORRECT_USERNAME_PASSWORD = "Incorrect username or password";
+    public static final String IMAGE_NOT_SAVED_TO_DIRECTORY = "Could not save image to the directory";
+    public static final String ASSISTANCE_ALREADY_EXISTS = "Assistance already exists";
+    public static final String NO_SUCH_ASSISTANCE_CATEGORY = "Assistance category does not exist";
+    public static final String SUBASSISTANCE_ALREADY_EXISTS = "Sub-assistance already exists";
 }
