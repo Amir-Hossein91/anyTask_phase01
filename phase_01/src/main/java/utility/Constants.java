46435551ceab6fc5f63693b3e30e5024c5c5e472
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-10 18:54:52 +0330

    phase 1: added Constants class

--- phase_01/src/main/java/utility/Constants.java
+++ phase_01/src/main/java/utility/Constants.java
@@ -1,4 +1,9 @@
 package utility;
 
 public class Constants {
+    public static final String INVALID_IMAGE_FORMAT = "Image format must be of type \"jpg\"";
+    public static final String INVALID_IMAGE_SIZE = "Image must have maximum size of 300 kB";
+    public static final String INCORRECT_PASSWORD = "Incorrect current password";
+    public static final String INVALID_USERNAME = "No person found with this username";
+    public static final String INCORRECT_USERNAME_PASSWORD = "Incorrect username or password";
 }
