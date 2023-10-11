9803f20e7cebeceeb01aa195182011ba7fddd362
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-11 23:24:41 +0330

    phase 1: changed LocalDate to LocalDateTime in TechnicianSuggestion

--- phase_01/src/main/java/entity/TechnicianSuggestion.java
+++ phase_01/src/main/java/entity/TechnicianSuggestion.java
@@ -8,7 +8,7 @@ import org.hibernate.validator.constraints.Range;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @SuperBuilder
@@ -21,11 +21,11 @@ import java.time.LocalDate;
 public class TechnicianSuggestion extends BaseEntity {
     @ManyToOne
     private Order order;
-    private LocalDate DateAndTimeOfTechSuggestion;
+    private LocalDateTime DateAndTimeOfTechSuggestion;
     @Range(min = 0, message = "Price can not be negative")
     private long techSuggestedPrice;
     @NotNull(message = "A technician suggested start date must be set")
-    private LocalDate techSuggestedDate;
+    private LocalDateTime techSuggestedDate;
     @Range(min = 0, message = "Task duration can not be negative")
     private int taskEstimatedDuration;
 }
