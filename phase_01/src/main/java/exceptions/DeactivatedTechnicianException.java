70b73b2e99d0b90807c7b4f5ba09dbbda02eebe5
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-11 15:22:18 +0330

    phase 1: two more exceptions added

---
+++ phase_01/src/main/java/exceptions/DeactivatedTechnicianException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class DeactivatedTechnicianException extends Exception{
+    public DeactivatedTechnicianException(String message) {
+        super(message);
+    }
+}
---
+++ phase_01/src/main/java/exceptions/DuplicateTechnicianException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class DuplicateTechnicianException extends Exception{
+    public DuplicateTechnicianException(String message) {
+        super(message);
+    }
+}
