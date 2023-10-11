0a4bd9e066dac7c9064ba65b0d4f30c7f43ba218
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-11 15:23:38 +0330

    phase 1: add and remove technicians to sub-assistance is implemented

--- phase_01/src/main/java/service/impl/TechnicianServiceImpl.java
+++ phase_01/src/main/java/service/impl/TechnicianServiceImpl.java
@@ -1,10 +1,14 @@
 package service.impl;
 
 import basics.baseService.impl.BaseServiceImpl;
-import entity.Technician;
+import entity.*;
 import entity.enums.TechnicianStatus;
+import exceptions.DeactivatedTechnicianException;
+import exceptions.DuplicateTechnicianException;
 import exceptions.InvalidImageException;
+import exceptions.NotFoundException;
 import repository.impl.TechnicianRepositoryImpl;
+import utility.ApplicationContext;
 import utility.Constants;
 
 import java.io.IOException;
@@ -15,8 +19,15 @@ import java.util.List;
 
 public class TechnicianServiceImpl extends BaseServiceImpl<TechnicianRepositoryImpl, Technician> {
 
+    private final PersonServiceImple personService;
+    private final SubAssistanceServiceImpl subAssistanceService;
+    private final AssistanceServiceImpl assistanceService;
+
     public TechnicianServiceImpl(TechnicianRepositoryImpl repository) {
         super(repository);
+        personService = ApplicationContext.personService;
+        subAssistanceService = ApplicationContext.subAssistanceService;
+        assistanceService = ApplicationContext.assistanceService;
     }
 
     public Technician specifyTechnician(Path path){
@@ -65,5 +76,71 @@ public class TechnicianServiceImpl extends BaseServiceImpl<TechnicianRepositoryI
         } catch (IOException e) {
             printer.printError(Constants.IMAGE_NOT_SAVED_TO_DIRECTORY);
         }
+    }
+
+    public void addTechnicianToSubAssistance(String managerName, String technicianName,
+                                             String subassistanceTitle,String assistanceTitle){
+        Person manager = personService.findByUsername(managerName);
+        if(manager instanceof Manager){
+            try{
+                Person technician = personService.findByUsername(technicianName);
+                Assistance assistance = assistanceService.findAssistance(assistanceTitle);
+
+                if(assistance == null)
+                    throw new NotFoundException(Constants.ASSISTANCE_NOT_FOUND);
+
+                SubAssistance subAssistance = subAssistanceService.findSubAssistance(subassistanceTitle,assistance);
+
+                if(!(technician instanceof Technician) || subAssistance == null)
+                    throw new NotFoundException(Constants.TECHINICIAN_OR_SUBASSISTANCE_NOT_FOUND);
+
+                if(!((Technician) technician).isActive())
+                    throw new DeactivatedTechnicianException(Constants.DEACTIVATED_TECHNICIAN);
+
+                List<Technician> technicians = subAssistance.getTechnicians();
+                if(technicians.contains(technician))
+                    throw new DuplicateTechnicianException(Constants.DUPLICATE_TECHNICIAN_SUBASSISTANCE);
+
+                technicians.add((Technician) technician);
+                ((Technician) technician).setTechnicianStatus(TechnicianStatus.APPROVED);
+                subAssistanceService.saveOrUpdate(subAssistance);
+
+            } catch (NotFoundException | DeactivatedTechnicianException | DuplicateTechnicianException e) {
+                printer.printError(e.getMessage());
+            }
+        }
+        else
+            printer.printError(("Only manager can add technicians to a sub-assistance"));
+    }
+
+    public void removeTechnicianFromSubAssistance(String managerName, String technicianName,
+                                             String subassistanceTitle,String assistanceTitle){
+        Person manager = personService.findByUsername(managerName);
+        if(manager instanceof Manager){
+            try{
+                Person technician = personService.findByUsername(technicianName);
+                Assistance assistance = assistanceService.findAssistance(assistanceTitle);
+
+                if(assistance == null)
+                    throw new NotFoundException(Constants.ASSISTANCE_NOT_FOUND);
+
+                SubAssistance subAssistance = subAssistanceService.findSubAssistance(subassistanceTitle,assistance);
+
+                if(!(technician instanceof Technician) || subAssistance == null)
+                    throw new NotFoundException(Constants.TECHINICIAN_OR_SUBASSISTANCE_NOT_FOUND);
+
+                List<Technician> technicians = subAssistance.getTechnicians();
+                if(!technicians.contains(technician))
+                    throw new NotFoundException(Constants.TECHNICIAN_NOT_IN_LIST);
+
+                technicians.remove((Technician) technician);
+                subAssistanceService.saveOrUpdate(subAssistance);
+
+            } catch (NotFoundException e) {
+                printer.printError(e.getMessage());
+            }
+        }
+        else
+            printer.printError(("Only manager can remove technicians from a sub-assistance"));
     }
 }
