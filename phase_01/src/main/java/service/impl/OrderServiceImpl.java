d338c9e31e3a14e39d8df544d41d85ec3504d4ea
Amir-Hossein91 <amir_ahmadi9034@yahoo.com>
2023-10-11 23:24:00 +0330

    phase 1: added the makeOrder method to OrderServiceImpl

--- phase_01/src/main/java/service/impl/OrderServiceImpl.java
+++ phase_01/src/main/java/service/impl/OrderServiceImpl.java
@@ -1,12 +1,104 @@
 package service.impl;
 
 import basics.baseService.impl.BaseServiceImpl;
-import entity.Order;
+import com.github.mfathi91.time.PersianDate;
+import entity.*;
+import entity.enums.OrderStatus;
+import exceptions.NotFoundException;
 import repository.impl.OrderRepositoryImpl;
+import repository.impl.PersonRepositoryImpl;
+import repository.impl.SubAssistanceRepositoryImpl;
+import utility.ApplicationContext;
+import utility.Constants;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class OrderServiceImpl extends BaseServiceImpl<OrderRepositoryImpl, Order> {
 
+    private final PersonServiceImple personService;
+    private final AssistanceServiceImpl assistanceService;
+    private final SubAssistanceServiceImpl subAssistanceService;
+
     public OrderServiceImpl(OrderRepositoryImpl repository) {
         super(repository);
+        PersonRepositoryImpl personRepository = new PersonRepositoryImpl(Person.class);
+        personService = new PersonServiceImple(personRepository);
+//        personService = ApplicationContext.personService;
+        assistanceService = ApplicationContext.assistanceService;
+        SubAssistanceRepositoryImpl subAssistanceRepository = new SubAssistanceRepositoryImpl(SubAssistance.class);
+        subAssistanceService = new SubAssistanceServiceImpl(subAssistanceRepository);
+//        subAssistanceService = ApplicationContext.subAssistanceService;
+    }
+
+    public void makeOrder(String customerUsername, String assistanceTitle, String subAssistanceTitle){
+        Person person = personService.findByUsername(customerUsername);
+        if( person instanceof Customer){
+            try{
+                Assistance assistance = assistanceService.findAssistance(assistanceTitle);
+                if(assistance == null)
+                    throw new NotFoundException(Constants.ASSISTANCE_NOT_FOUND);
+
+                SubAssistance subAssistance = subAssistanceService.findSubAssistance(subAssistanceTitle,assistance);
+                if(subAssistance == null)
+                    throw new NotFoundException(Constants.NO_SUCH_SUBASSISTANCE);
+
+                OrderDescription orderDescription = createOrderDescription(subAssistance);
+
+                Order order = Order.builder().subAssistance(subAssistance).customer((Customer) person)
+                        .orderRegistrationDateAndTime(LocalDateTime.now()).orderDescription(orderDescription)
+                        .orderStatus(OrderStatus.WAITING_FOR_TECHNICIANS_SUGGESTIONS)
+                        .technicianScore(1).build();
+                saveOrUpdate(order);
+            } catch (NotFoundException | DateTimeException | IllegalArgumentException e) {
+                printer.printError(e.getMessage());
+            }
+        }
+        else
+            printer.printError("Only a customer can make an order");
+    }
+
+    private OrderDescription createOrderDescription(SubAssistance subAssistance){
+        long basePrice = subAssistance.getBasePrice();
+        OrderDescription orderDescription = null;
+        while(orderDescription == null){
+            try{
+                printer.getInput("suggested price");
+                long suggestedPrice = input.nextLong();
+                input.nextLine();
+                if(suggestedPrice < basePrice)
+                    throw new IllegalArgumentException(Constants.INVALID_SUGGESTED_PRICE);
+                printer.getInput("desired year");
+                int year = input.nextInt();
+                input.nextLine();
+                printer.getInput("desired month");
+                int month = input.nextInt();
+                input.nextLine();
+                printer.getInput("desired day");
+                int day = input.nextInt();
+                input.nextLine();
+                printer.getInput("desired hour");
+                int hour = input.nextInt();
+                input.nextLine();
+                printer.getInput("desired minute");
+                int minute = input.nextInt();
+                input.nextLine();
+                PersianDate persionDate = PersianDate.of(year,month,day);
+                LocalDate gregorianDate = persionDate.toGregorian();
+                LocalDateTime dateTime = gregorianDate.atTime(hour,minute);
+                if(dateTime.isBefore(LocalDateTime.now()))
+                    throw new DateTimeException(Constants.DATE_BEFORE_NOW);
+                printer.getInput("task details");
+                String details = input.nextLine();
+                printer.getInput("address");
+                String address = input.nextLine();
+                orderDescription = OrderDescription.builder().customerSuggestedPrice(suggestedPrice)
+                        .customerDesiredDateAndTime(dateTime).taskDetails(details).address(address).build();
+            } catch (DateTimeException | IllegalArgumentException e){
+                printer.printError(e.getMessage());
+            }
+        }
+        return orderDescription;
     }
 }
